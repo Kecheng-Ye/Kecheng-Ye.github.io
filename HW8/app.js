5bb08724adfef41c28d31a5835d5ad76441b5b48
Kecheng-Ye <kevin_ye1997@outlook.com>
2022-03-31 21:57:27 -0600

    fix: routing crash issue

--- HW8/app.js
+++ HW8/app.js
@@ -2,20 +2,24 @@ import express from "express";
 import path from "path";
 import * as util from "./util.js";
 import { api } from "./backend/server.js";
-import {fileURLToPath} from 'url';
 
-const __filename = fileURLToPath(import.meta.url);
 const app = express();
 const port = process.env.PORT || 5555;
-app.use(express.json())
-app.use(express.static('public'))
-app.use(express.static(path.join(process.cwd(), "/frontend/stock-app/dist/stock-app")));
+app.use(express.json());
+app.use(express.static("public"));
+app.use(
+  express.static(path.join(process.cwd(), "/frontend/stock-app/dist/stock-app"))
+);
 
+// intialize backend API
+const backend = new api(app, "/api");
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(process.cwd(), "/frontend/stock-app/dist/stock-app/index.html"));
+// initialize frontend page
+app.all("/*", (req, res) => {
+  res.sendFile(
+    path.join(process.cwd(), "/frontend/stock-app/dist/stock-app/index.html")
+  );
 });
 
-const backend = new api(app, "/api");
 app.listen(port);
 console.log(`Start App at ${port}`);
